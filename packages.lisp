d4a9fe8fce5448217438c0453a9a76c25add6fe9
the Phoeron <thephoeron@users.noreply.github.com>
2015-05-13 11:25:31 -0400

    Update packages.lisp

--- packages.lisp
+++ packages.lisp
@@ -2,4 +2,18 @@
 
 (in-package :cl-user)
 
+(defpackage #:cl-bootstrap
+  (:nicknames #:bootstrap)
+  (:use :cl
+        :cl-user
+        :cl-who
+        :cl-css
+        :parenscript)
+  (:shadowing-import-from :parenscript #:%)
+  (:export #:*cl-bootstrap-version*))
+
+(in-package :cl-bootstrap)
+
+(defparameter *cl-bootstrap-version* #.cl-bootstrap-asd:*cl-bootstrap-version*)
+
 ;; EOF
