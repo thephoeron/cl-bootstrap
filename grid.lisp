6c514c398752504c286350d0d60489c7efbce345
the Phoeron <thephoeron@users.noreply.github.com>
2015-07-31 09:54:30 -0400

    Create grid.lisp

---
+++ grid.lisp
@@ -0,0 +1,37 @@
+;; file: grid.lisp
+
+(in-package :cl-bootstrap)
+
+(defmacro container ((&key (fluid nil)) &body body)
+  "Bootstrap Grid Container. Set :fluid t for fluid layout."
+  `(cl-who:with-html-output (hunchentoot::*standard-output*)
+     (:div :class ,(if fluid "container-fluid" "container")
+       ,@body)))
+
+(defmacro row (&body body)
+  "Bootstrap Grid Row."
+  `(cl-who:with-html-output (hunchentoot::*standard-output*)
+     (:div :class "row"
+       ,@body)))
+
+(defmacro col ((&key (vp "xs") (w 1) (offset nil)) &body body)
+  "Bootstrap Grid Column. Set viewport :vp to xs, sm, md, or lg; width :w to 1 -- 12. If you need to add an offset, set :offset <column offset number>."
+  `(cl-who:with-html-output (hunchentoot::*standard-output*)
+     ,(if offset
+          `(:div :class ,(format nil "col-~A-~S col-~A-offset-~S" vp w vp offset)
+             ,@body)
+          ; else
+          `(:div :class ,(format nil "col-~A-~S" vp w)
+             ,@body))))
+
+(defmacro grid ((&key (fluid nil) (vp "xs")) &rest rest)
+  "Define a Bootstrap grid with minimal syntax."
+  `(container :fluid ,fluid
+     ,@(loop for row in rest
+             collect (row (loop for column in row
+                                with l = (length row)
+                                with w = (floor 12 l)
+                                collect (col (:vp vp :w w)
+                                          column))))))
+
+;; EOF
